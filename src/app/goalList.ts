97d80a3ef7d5837dffb47fee90a1db5c0ff7a73b
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-12 05:21:44 +0300

    Create goalList folder to store our goals

---
+++ src/app/goalList.ts
@@ -0,0 +1,11 @@
+import {Goal} from './goal'
+
+export let Goals:Goal[] = [
+
+  new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2019,9,10)),
+  new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2021,11,10)),
+  new Goal(3,'Get new Phone Case','Alphonce has his birthday coming up soon', new Date(2021,9,10)),
+  new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2022,0,10)),
+  new Goal(5,'Solve math homework','Damn Math', new Date(2021,10,10)),
+  new Goal(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2022,7,10)),
+];
