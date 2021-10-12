d398c6ea41e6c70d3c7cf20313e259b937bc4371
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-12 06:03:49 +0300

    Add err function to handle the occurrence of an error

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -56,7 +56,11 @@ export class GoalComponent implements OnInit {
     }
 
     this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
-      this.quote= new Quote(data.author, data.quote)
+      // Successful Api request
+      this.quote= new Quote(data.author, data.quote) 
+    }, err=>{
+      this.quote= new Quote("Winston Churchill","Never never give up!");
+      console.log("An error occurred");
     })
   }
 
