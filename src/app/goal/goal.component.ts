b21e8030c0cd5d46ee4b708292feba3e6c170295
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-12 05:29:57 +0300

    Delete goals , import alert and goal services. Apply the services

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,45 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import{Goal} from '../goal'
+import {Goal} from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
 
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css'],
-  styles:['h1{color: #ffd900}']
+  styles:['h1{color: #ffd900}'],
+  providers:[GoalService,AlertService]
 })
 export class GoalComponent implements OnInit {
-  goals:Goal[] = [
 
-  new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2019,9,10)),
-  new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2021,11,10)),
-  new Goal(3,'Get new Phone Case','Alphonce has his birthday coming up soon', new Date(2021,9,10)),
-  new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2022,0,10)),
-  new Goal(5,'Solve math homework','Damn Math', new Date(2021,10,10)),
-  new Goal(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2022,7,10)),
-];
-
-toggleDetails(index:any){
-  this.goals[index].showDescription = !this.goals[index].showDescription;
-}
-addNewGoal(goal: Goal){
-  let goalLength = this.goals.length;
-  goal.id = goalLength+1;
-  goal.completeDate = new Date(goal.completeDate)
-  this.goals.push(goal)
-}
-deleteGoal(isComplete: any, index: number){
-  if (isComplete) {
-    let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
-    
-    if(toDelete){
-      this.goals.splice(index,1);
-  
-    }
+  goals!:Goal[];
+  alertService:AlertService;
+  addNewGoal(goal: Goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
   }
-  
-}
-  constructor() { }
+
+
+  toggleDetails(index:any){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  deleteGoal(isComplete: any, index: number){
+    if (isComplete) {
+      let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      
+      if(toDelete){
+        this.goals.splice(index,1);
+        this.alertService.alertMe("The goal has been deleted")
+    
+      }
+    }
+    
+  }
+  constructor(goalService:GoalService,alertService:AlertService) {
+    this.goals = goalService.getGoals();
+    this.alertService=alertService;
+   }
 
   ngOnInit(): void {
   }
